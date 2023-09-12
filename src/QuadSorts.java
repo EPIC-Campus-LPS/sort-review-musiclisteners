ccea6c5de76ca732cad865026af53314cb72652c
nxthyn <nathanh2362\@lpsk12.org>
2023-09-12 10:28:22 -0600

    Created QuadSorts class and created empty sorting methods

---
+++ src/QuadSorts.java
@@ -0,0 +1,25 @@
+public class QuadSorts {
+
+    public static void bubbleSort(int[] array) {
+
+    }
+
+    public static void selectionSort(int[] array) {
+
+
+
+    }
+
+    public static void insertionSort(int[] array) {
+
+
+
+    }
+
+    public static void mergeSort(int[] array) {
+
+
+
+    }
+
+}
