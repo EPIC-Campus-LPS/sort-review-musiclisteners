5fa2ee3a3bd1264469858f6d6206222b5a2247da
nxthyn <nathanh2362\@lpsk12.org>
2023-09-12 11:12:34 -0600

    started working on insertionSort() method

--- src/QuadSorts.java
+++ src/QuadSorts.java
@@ -42,13 +42,48 @@ public class QuadSorts {
 
     public static void selectionSort(int[] array) {
 
+        for (int i = 0; i < array.length-1; i++) {
 
+            int small = array[i];
+            int place = i;
+            for (int j = i+1; j < array.length; j++) {
+
+                if (array[j] < small) {
+
+                    small = array[j];
+                    place = j;
+
+                }
+
+            }
+
+            if (small == array[i]) {
+
+                continue;
+
+            }
+            array[place] = array[i];
+            array[i] = small;
+
+        }
 
     }
 
     public static void insertionSort(int[] array) {
 
+        for (int i = 1; i < array.length; i++) {
 
+            int k = array[i];
+            int j = i-1;
+
+            while (j >= 0 && array[j] > k) {
+
+                array[j+1] = array[j];
+                j--;
+
+            }
+
+        }
 
     }
 
