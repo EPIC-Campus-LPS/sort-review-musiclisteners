3ba86e8f615bf2a7ab7c4a75943d25407bc83f64
nxthyn <nathanh2362\@lpsk12.org>
2023-09-12 10:45:45 -0600

    completed bubbleSort method

--- src/QuadSorts.java
+++ src/QuadSorts.java
@@ -2,6 +2,42 @@ public class QuadSorts {
 
     public static void bubbleSort(int[] array) {
 
+        boolean solved = false;
+        while (!solved) {
+
+            for (int i = 0;i < array.length-1; i++) {
+
+                if (array[i] > array[i+1]) {
+
+                    solved = false;
+                    break;
+
+                } else {
+
+                    solved = true;
+
+                }
+
+            }
+
+            if (!solved) {
+
+                for (int j = 0; j < array.length-1; j++) {
+
+                    if (array[j] > array[j+1]) {
+
+                        int temp = array[j];
+                        array[j] = array[j+1];
+                        array[j+1] = temp;
+
+                    }
+
+                }
+
+            }
+
+        }
+
     }
 
     public static void selectionSort(int[] array) {
